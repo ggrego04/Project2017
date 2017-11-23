ddca916d2fdb2b59372120bfc13453b00466781b
cachil01 <33749663+cachil01@users.noreply.github.com>
2017-11-23 19:44:01 +0200

    Exception handling

--- TOEFLSolver.java
+++ TOEFLSolver.java
@@ -4,45 +4,21 @@ import java.util.Scanner;
 
 public class TOEFLSolver {
 
-	
-	public void getSentenceLists(String text){
-		
-		
-		
-		
-		
+	public void getSentenceLists(String text) {
+
 	}
-	
-	
-	
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) throws FileNotFoundException {
+
 		File file1 = new File("pg2600");
-		File file2 =  new File("pg7178");
-		
-		
-		
-		try {
-			Scanner scan1 = new Scanner(file1);
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-		}
-		
-		try {
-			Scanner scan2 = new Scanner(file2);
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-		}
-		
-		
-		
-		
+		File file2 = new File("pg7178");
+
+		Scanner scan1 = new Scanner(file1);
+
+		Scanner scan2 = new Scanner(file2);
 		
 		
 		
 		
 	}
 }
-	
