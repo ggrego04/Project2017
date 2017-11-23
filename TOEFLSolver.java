0677591707f1b5697a97017ed39f1c4c592f5d16
cachil01 <33749663+cachil01@users.noreply.github.com>
2017-11-23 19:20:04 +0200

    scanner

--- TOEFLSolver.java
+++ TOEFLSolver.java
@@ -1,4 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class TOEFLSolver {
 
-}
+	public static void main(String[] args) {
+		
+		File file1 = new File("pg2600");
+		File file2 =  new File("pg7178");
+		
+		
+		
+		try {
+			Scanner scan1 = new Scanner(file1);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+		try {
+			Scanner scan2 = new Scanner(file2);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
