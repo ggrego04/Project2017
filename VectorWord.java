07041bb7334ea0ea981c9172f2198b81121e7c44
ggrego04 <33683914+ggrego04@users.noreply.github.com>
2017-11-23 19:51:14 +0200

    VW
    constr

--- VectorWord.java
+++ VectorWord.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class VectorWord {
-
+	ArrayList<Integer> vector = new ArrayList<Integer>();
+	
+	public VectorWord(int...i) {
+		for(int o:i) {
+			vector.add(o);
+		}
+	}
 }
