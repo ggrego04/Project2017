3dfd4816385451eed0cec82f51bb30e28ff97588
ggrego04 <33683914+ggrego04@users.noreply.github.com>
2017-11-23 20:02:33 +0200

    VW
    new constructor VectorWord p pianni mesa allon array list me words
    jai alla3a jai ta onomata tn metavliton se sentences jai text

--- VectorWord.java
+++ VectorWord.java
@@ -3,11 +3,16 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class VectorWord {
-	ArrayList<Word> vector1 = new ArrayList<Word>();
-	
+	ArrayList<Word> sentences = new ArrayList<Word>();
+	ArrayList<VectorWord> text=new ArrayList<VectorWord>();
 	public VectorWord(Word...s) {
 		for(Word o:s) {
-			vector1.add(o);
+			sentences.add(o);
+		}
+	}
+	public VectorWord(VectorWord...s) {
+		for(VectorWord o:s) {
+			text.add(o);
 		}
 	}
 }
